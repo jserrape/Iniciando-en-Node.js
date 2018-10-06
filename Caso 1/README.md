68839a8d3ad32757b4b3162ee00f8f934d2f9101
xenahort <juan.carlos.wow.95@gmail.com>
2018-10-06 14:41:34 +0200

    Enviar y mostrar un JSON

---
+++ Caso 1/README.md
@@ -0,0 +1,1 @@
+# Hacer un Hola Mundo!
--- Caso 3/hola_html.js
+++ Caso 3/hola_html.js
@@ -5,7 +5,8 @@ var fs = require("fs"); /* Se encarga de comunicarse con el sistema de archivos 
 
 http.createServer(function(req,res){
 	fs.readFile("./index.html", function(err,html){
-		res.write(html);
+		res.writeHead(200,{"Content-Type":"application/json"});
+		res.write(JSON.stringify({nombre: "Juan Carlos", apellidos: "Serrano Pérez"}));
 		res.end();
 	});
 }).listen(8080);
